659e29f6cdcd07fb29c9e4301cd133457a284d8f
pemburunasikotak <32323599+mynameistj@users.noreply.github.com>
2019-12-21 22:26:01 +0700

    update Tj ke 1

---
+++ app/src/main/java/com/example/uass/ActivityLogin.kt
@@ -0,0 +1,13 @@
+package com.example.uass
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ActivityLogin: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+    }
+}
---
+++ app/src/main/java/com/example/uass/ActivitySmp.kt
@@ -0,0 +1,4 @@
+package com.example.uass
+
+class ActivitySmp {
+}
