129e75c4c0116b17b09e9f61608b99daa4b909d8
pemburunasikotak <32323599+mynameistj@users.noreply.github.com>
2019-12-21 22:09:11 +0700

    update Tj fungsi main

---
+++ app/src/main/java/com/example/uass/ActivitySd.kt
@@ -0,0 +1,11 @@
+package com.example.uass
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ActivitySd: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sd)
+    }
+}
---
+++ app/src/main/java/com/example/uass/SplesScrean.kt
@@ -0,0 +1,29 @@
+package com.example.uass
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+class SplesScrean: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //hiding title bar of this activity
+        window.requestFeature(Window.FEATURE_NO_TITLE)
+        //making this activity full screen
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.spalsh_screen)
+
+        //4second splash time
+        Handler().postDelayed({
+            //start main activity
+            startActivity(Intent(this@SplesScrean, MainActivity::class.java))
+            //finish this activity
+            finish()
+        },4000)
+
+    }
+}
